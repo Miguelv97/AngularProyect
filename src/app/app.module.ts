87f6aa403d8390e94a282114482d671777865484
Miguelv97 <migueldavidvidalguerrero@gmail.com>
2022-11-27 15:14:07 -0500

    Creacios de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { YouTubePlayerModule } from '@angular/youtube-player';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { TestimoniosComponent } from './components/testimonios/testimonios.component';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { VideoComponent } from './components/video/video.component';
+
+
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
+  //{path: 'Video', component: VideoComponent},
+  { path: 'Inicio', component: InicioComponent },
+  { path: 'QuienesSomos', component: QuienesSomosComponent },
+  { path: 'Servicios', component: ServiciosComponent },
+  { path: 'Testimonios', component: TestimoniosComponent },
+  { path: 'Contactanos', component: ContactanosComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AsideComponent,
+    InicioComponent,
+    QuienesSomosComponent,
+    ServiciosComponent,
+    TestimoniosComponent,
+    ContactanosComponent,
+    VideoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    YouTubePlayerModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  GitHub:string = 'https://github.com/Miguelv97';
+  Avast:string = 'https://www.avast.com/es-co/about#pc'
+}
